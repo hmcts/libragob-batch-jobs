5791a06992a85ceda3c65af23eb17ea034663795
djporta <157360643+djporta@users.noreply.github.com>
2024-10-07 14:40:43 +0100

    Update 9aAZUREDB_AMD_confiscation_recon_RRID.sql

--- sql/9aAZUREDB_AMD_confiscation_recon_RRID.sql
+++ sql/9aAZUREDB_AMD_confiscation_recon_RRID.sql
@@ -1,1 +1,1 @@
-\copy (select max(RR_ID) from RECONCILIATION_RUNS) To '/tmp/ams-reporting/9aAZUREDB_AMD_con***REMOVED***cation_recon_RRID.csv' With CSV DELIMITER ','
+\copy (select max(RR_ID) from RECONCILIATION_RUNS) To '/tmp/ams-reporting/9aAZUREDB_AMD_con***REMOVED***scation_recon_RRID.csv' With CSV DELIMITER ','
