e971bf758eb728ae9984796bb2d902ad522ea546
djporta <157360643+djporta@users.noreply.github.com>
2025-01-30 07:47:30 +0000

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ############################################################### This is the AMD AzureDB HealthCheck script, and the associated documentation is in Ensemble under the "Libra System Admin Documents" area:
 ############################################################### "GoB Phase 1 - Oracle_Postgres DB Checks_v11.8_MAP.docx" is the latest version as of 22/01/2025
-echo "Script Version 21.3 rec x3 override"
+echo "Script Version 21.4 rec x2 override"
 echo "Designed by Mark A. Porter"
 
 if [[ `echo $KV_NAME | grep "test"` ]];then
@@ -1181,6 +1181,8 @@ echo "21/01/2025.*fines_recon_status" >> $override_file
 echo "27/01/2025.*_recon_status" >> $override_file
 
 echo "29/01/2025.*_recon_status" >> $override_file
+
+echo "30/01/2025.*(confiscation|fines)_recon_status" >> $override_file
 
 fi
 
