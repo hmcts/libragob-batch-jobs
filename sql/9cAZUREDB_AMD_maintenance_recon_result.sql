380af49a6b528822b5f5b1e30695c935ea0764f3
djporta <157360643+djporta@users.noreply.github.com>
2025-02-12 14:48:25 +0000

    Update and rename 9cAZUREDB_AMD_maintenance_recon_result.sql to 9AZUREDB_AMD_maintenance_recon_result.sql

--- sql/9cAZUREDB_AMD_maintenance_recon_result.sql
+++
@@ -1,1 +0,0 @@
-\copy (select 'maintenance' || rr_id,date(completed_date),count(*) from RECONCILIATION_RUNS where error_count = 0 group by rr_id,date(completed_date) order by rr_id desc limit 10) To '/tmp/ams-reporting/9aAZUREDB_AMD_maintenance_recon_result.csv' With CSV DELIMITER ','
