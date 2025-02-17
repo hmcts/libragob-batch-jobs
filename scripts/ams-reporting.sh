fbb3ac0b062a6abda6ffb6e82440514ff17b3f9f
djporta <157360643+djporta@users.noreply.github.com>
2025-02-17 13:44:24 +0000

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ############################################################### This is the AMD AzureDB HealthCheck script, and the associated documentation is in Ensemble under the "Libra System Admin Documents" area:
 ############################################################### "GoB Phase 1 - Oracle_Postgres DB Checks_v11.9_MAP.docx" is the latest version as of 04/02/2025
-echo "Script Version 21.9 AESD single loop"
+echo "Script Version 21.9 rec sorting"
 echo "Designed by Mark A. Porter"
 
 if [[ `echo $KV_NAME | grep "test"` ]];then
@@ -500,14 +500,18 @@ for cnt in 1 2 3;do
   fi
 echo "-------------------------------------"
 cat ${OPDIR}9AZUREDB_AMD_${dbname_str}_recon_result.csv
+echo "-------------SORT--------------------"
+cat ${OPDIR}9AZUREDB_AMD_${dbname_str}_recon_result.csv | sort -k 2 -t ","
+
   for loopc in 0 1 2 3 4 5 6 7 8 9;do
     op_date=`date "+%Y-%m-%d" -d "-${loopc} days"`
+    rec_line=`grep -P "$dbname_str.*$op_date" ${OPDIR}9AZUREDB_AMD_${dbname_str}_recon_result.csv | head -1`
 
-    if [[ `grep -P "$dbname_str.*$op_date" ${OPDIR}9AZUREDB_AMD_${dbname_str}_recon_result.csv` ]];then
-      rr_id=`grep -P "$dbname_str.*$op_date" ${OPDIR}9AZUREDB_AMD_${dbname_str}_recon_result.csv | awk -F"," '{print $1}' | awk -F"_" '{print $2}'`
-      rr_cnt=`grep -P "$dbname_str.*$op_date" ${OPDIR}9AZUREDB_AMD_${dbname_str}_recon_result.csv | awk -F"," '{print $3}'`
+    if [[ $rec_line ]];then
+      rr_id=`echo $rec_line | awk -F"," '{print $1}' | awk -F"_" '{print $2}'`
+      rr_cnt=`echo $rec_line | awk -F"," '{print $3}'`
 
-      if [[ `grep -P "$dbname_str.*$op_date.*,${rec_rows}$" ${OPDIR}9AZUREDB_AMD_${dbname_str}_recon_result.csv` ]];then
+      if [[ `grep -P "$dbname_str.*$op_date.*,${rec_rows}$" ${OPDIR}9AZUREDB_AMD_${dbname_str}_recon_result.csv | head -1` ]];then
         echo "$(date "+%d/%m/%Y %T"),$dbname_str RR_ID=$rr_id ROWS=$rr_cnt/$rec_rows,DATE=$op_date,ok" >> $OUTFILE
 
         if [[ $loopc < 4 ]];then
