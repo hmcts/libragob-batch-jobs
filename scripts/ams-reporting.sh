960b32a1194c49e24c4e55db79a5a61cf79f7c9c
petercockburn <168640160+petercockburn@users.noreply.github.com>
2025-03-10 08:08:31 +0000

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -1076,6 +1076,8 @@ echo "24/02/2025.*AZDB_overall_recon_status" >> $override_file
 
 echo "25/02/2025.*AZDB_overall_recon_status" >> $override_file
 
+echo "10/03/2025.*AZDB_overall_recon_status" >> $override_file
+
 echo "26/02/2025.*AZDB_update_processing_backlog82" >> $override_file
 
 # 28 & 105 data errors
