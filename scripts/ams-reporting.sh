46b3a29603355238c6f5397fb57652b8450eb4df
djporta <157360643+djporta@users.noreply.github.com>
2025-03-31 12:56:51 +0100

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -1121,8 +1121,6 @@ echo "28/03/2025.*AZDB_update_processing_backlog77" >> $override_file
 
 echo "31/03/2025.*AZDB_update_processing_backlog77" >> $override_file
 
-echo "31/03/2025.*AZDB_update_processing_backlog124" >> $override_file
-
 echo "recon_status" >> $override_file
 
 fi
