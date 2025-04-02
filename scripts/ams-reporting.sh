f40fe06fdc04dbbb8aaaa2e89b9c159ff5dc52ab
djporta <157360643+djporta@users.noreply.github.com>
2025-04-02 13:11:57 +0100

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ############################################################### This is the AMD AzureDB HealthCheck script, and the associated documentation is in Ensemble under the "Libra System Admin Documents" area:
 ############################################################### "GoB Phase 1 - Oracle_Postgres DB Checks_v11.9_MAP.docx" is the latest version as of 27/02/2025
-echo "Script Version 24.1 130 permanent 77 Check #6 override"
+echo "Script Version 24.1 removed op_date fudge"
 echo "Designed by Mark A. Porter"
 
 if [[ `echo $KV_NAME | grep "test"` ]];then
@@ -565,7 +565,6 @@ op_date=`date "+%Y-%m-%d"`
 op_date1=`date "+%Y-%m-%d" -d "-1 days"`
 op_date2=`date "+%Y-%m-%d" -d "-2 days"`
 op_date3=`date "+%Y-%m-%d" -d "-3 days"`
-op_date3=$op_date2
 #op_date1=`date "+%Y-%m-%d"`
 #op_date2=`date "+%Y-%m-%d"`
 #op_date3=`date "+%Y-%m-%d"`
