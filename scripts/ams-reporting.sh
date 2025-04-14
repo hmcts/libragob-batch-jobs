078adf635b2e121f74ead9b03d8422f807525f83
djporta <157360643+djporta@users.noreply.github.com>
2025-04-14 09:42:25 +0100

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -572,9 +572,9 @@ while read -r met;do
 done < ${OPDIR}confiscation_mets
 
 if [[ `echo $met_no_good_result_list` ]];then
-  echo "$(date "+%d/%m/%Y %T"),AZDB_overall_confiscation_recon_status,Check rec history by means of the two last queries in Section #9 of JBOX C:\Libra\MarkP\sql.txt: $met_no_good_result_list,warn" >> $OUTFILE
+  echo "$(date "+%d/%m/%Y %T"),AZDB_overall_confiscation_recon_status,Check rec history by means of the two last queries in Check #9 on JBOX C:\Libra\MarkP\sql.txt: $met_no_good_result_list,warn" >> $OUTFILE
 elif [[ `echo $met_recon_errors_list` ]];then
-  echo "$(date "+%d/%m/%Y %T"),AZDB_overall_confiscation_recon_status,Check rec history by means of the two last queries in Section #9 of JBOX C:\Libra\MarkP\sql.txt: $met_recon_errors_list,warn" >> $OUTFILE
+  echo "$(date "+%d/%m/%Y %T"),AZDB_overall_confiscation_recon_status,Check rec history by means of the two last queries in Check #9 on JBOX C:\Libra\MarkP\sql.txt: $met_recon_errors_list,warn" >> $OUTFILE
 else
   echo "$(date "+%d/%m/%Y %T"),AZDB_overall_confiscation_recon_status,All METs have seen a successful rec in the last 4 days,ok" >> $OUTFILE
 fi
@@ -624,9 +624,9 @@ fi
 done < ${OPDIR}fines_mets
 
 if [[ `echo $met_no_good_result_list` ]];then
-  echo "$(date "+%d/%m/%Y %T"),AZDB_overall_fines_recon_status,Check rec history by means of the two last queries in Section #9 of JBOX C:\Libra\MarkP\sql.txt: $met_no_good_result_list,warn" >> $OUTFILE
+  echo "$(date "+%d/%m/%Y %T"),AZDB_overall_fines_recon_status,Check rec history by means of the two last queries in Check #9 on JBOX C:\Libra\MarkP\sql.txt: $met_no_good_result_list,warn" >> $OUTFILE
 elif [[ `echo $met_recon_errors_list` ]];then
-  echo "$(date "+%d/%m/%Y %T"),AZDB_overall_fines_recon_status,Check rec history by means of the two last queries in Section #9 of JBOX C:\Libra\MarkP\sql.txt: $met_recon_errors_list,warn" >> $OUTFILE
+  echo "$(date "+%d/%m/%Y %T"),AZDB_overall_fines_recon_status,Check rec history by means of the two last queries in Check #9 on JBOX C:\Libra\MarkP\sql.txt: $met_recon_errors_list,warn" >> $OUTFILE
 else
   echo "$(date "+%d/%m/%Y %T"),AZDB_overall_fines_recon_status,All METs have seen a successful rec in the last 4 days,ok" >> $OUTFILE
 fi
@@ -645,9 +645,9 @@ while read -r met;do
 done < ${OPDIR}maintenance_mets
 
 if [[ `echo $met_no_good_result_list` ]];then
-  echo "$(date "+%d/%m/%Y %T"),AZDB_overall_maintenance_recon_status,Check rec history by means of the two last queries in Section #9 of JBOX C:\Libra\MarkP\sql.txt: $met_no_good_result_list,warn" >> $OUTFILE
+  echo "$(date "+%d/%m/%Y %T"),AZDB_overall_maintenance_recon_status,Check rec history by means of the two last queries in Check #9 on JBOX C:\Libra\MarkP\sql.txt: $met_no_good_result_list,warn" >> $OUTFILE
 elif [[ `echo $met_recon_errors_list` ]];then
-  echo "$(date "+%d/%m/%Y %T"),AZDB_overall_maintenance_recon_status,Check rec history by means of the two last queries in Section #9 of JBOX C:\Libra\MarkP\sql.txt: $met_recon_errors_list,warn" >> $OUTFILE
+  echo "$(date "+%d/%m/%Y %T"),AZDB_overall_maintenance_recon_status,Check rec history by means of the two last queries in Check #9 on JBOX C:\Libra\MarkP\sql.txt: $met_recon_errors_list,warn" >> $OUTFILE
 else
   echo "$(date "+%d/%m/%Y %T"),AZDB_overall_maintenance_recon_status,All METs have seen a successful rec in the last 4 days,ok" >> $OUTFILE
 fi
