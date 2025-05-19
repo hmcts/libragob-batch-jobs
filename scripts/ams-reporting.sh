1d3f023d696dd72258a9f825eeff4a7e6305e582
djporta <157360643+djporta@users.noreply.github.com>
2025-05-19 20:26:35 +0100

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ############################################################### This is the AMD AzureDB HealthCheck script, and the associated documentation is in Ensemble under the "Libra System Admin Documents" area:
 ############################################################### "GoB Phase 1 - Oracle_Postgres DB Checks_v11.9_MAP.docx" is the latest version as of 27/02/2025
-echo "Script Version 25.4 H/K fix"
+echo "Script Version 25.4 H/K fix and dupe seq num if"
 echo "Designed by Mark A. Porter"
 
 if [[ `echo $KV_NAME | grep "test"` ]];then
@@ -1131,8 +1131,8 @@ echo "$(date "+%d/%m/%Y %T") Connecting to $event_db database" >> $OUTFILE_LOG
 psql "sslmode=require host=${event_host} dbname=${event_db} port=${event_port} user=${event_username} password=${event_password}" --file=/sql/12AZUREDB_AMD_ora_rowscn_bug_seq_nums.sql
 echo "$(date "+%d/%m/%Y %T") SQL for Check #12 has been run" >> $OUTFILE_LOG
 
-#if [[ -f ${OPDIR}12AZUREDB_AMD_ora_rowscn_bug_seq_nums.csv ]];then
-#  dupe_seq_nums_linecount=`cat ${OPDIR}12AZUREDB_AMD_ora_rowscn_bug_seq_nums.csv | wc -l | xargs`
+#if [[ `cat ${OPDIR}12AZUREDB_AMD_ora_rowscn_bug_seq_nums.csv | wc -l` ]];then
+#  dupe_seq_nums_linecount=`cat ${OPDIR}12AZUREDB_AMD_ora_rowscn_bug_seq_nums.csv | wc -l`
 #  check12_sp_start_time=$(date "+%H:%M:%S %a %b %e %Y")
 #  epoch_secs_check12_sp_start_time=$(date '+%s' -d "$check12_sp_start_time")
 #  echo "$(date "+%d/%m/%Y %T") SQL for Check #12 for duplcate sequence number fix is about to be run: call fix_duplicate_seq_nos()" >> $OUTFILE_LOG
