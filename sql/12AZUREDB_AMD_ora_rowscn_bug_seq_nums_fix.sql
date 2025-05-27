031370e9285765a2ed74134f77fbb500ba0c8710
djporta <157360643+djporta@users.noreply.github.com>
2025-05-27 10:37:25 +0100

    Update 12AZUREDB_AMD_ora_rowscn_bug_seq_nums_fix.sql

--- sql/12AZUREDB_AMD_ora_rowscn_bug_seq_nums_fix.sql
+++ sql/12AZUREDB_AMD_ora_rowscn_bug_seq_nums_fix.sql
@@ -1,1 +1,1 @@
-\copy {CALL public.fix_duplicate_seq_nos()} To '/tmp/ams-reporting/12AZUREDB_AMD_ora_rowscn_bug_seq_nums_fix.csv' With CSV DELIMITER ','
+\copy 'CALL fix_duplicate_seq_nos()' To '/tmp/ams-reporting/12AZUREDB_AMD_ora_rowscn_bug_seq_nums_fix.csv' With CSV DELIMITER ','
