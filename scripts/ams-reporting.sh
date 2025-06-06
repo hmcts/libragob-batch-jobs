e7353d1f6e754ec51de5e6bf056ab0ebc6a7fed6
petercockburn <168640160+petercockburn@users.noreply.github.com>
2025-06-06 07:26:37 +0100

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -1328,6 +1328,7 @@ echo "16/04/2025.*AZDB_fines_recon_status" >> $override_file
 echo "22/04/2025.*AZDB_fines_recon_status" >> $override_file
 echo "04/06/2025.*AZDB_fines_recon_status" >> $override_file
 echo "05/06/2025.*AZDB_fines_recon_status" >> $override_file
+echo "06/06/2025.*AZDB_fines_recon_status" >> $override_file
 
 
 echo "17/04/2025.*AZDB_update_processing_backlog77" >> $override_file
