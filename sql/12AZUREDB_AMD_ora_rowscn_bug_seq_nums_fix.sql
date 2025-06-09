24ac05455e2eeba026f60af71e291a197baeafc1
Alex Bance <Alex.Bance@justice.gov.uk>
2025-06-09 10:26:36 +0100

    fix: correct overzelous removals

--- sql/12AZUREDB_AMD_ora_rowscn_bug_seq_nums_fix.sql
+++ sql/12AZUREDB_AMD_ora_rowscn_bug_seq_nums_fix.sql
@@ -1,1 +1,1 @@
-\copy (call ***REMOVED***x_duplicate_seq_nos()) To '/tmp/ams-reporting/12AZUREDB_AMD_ora_rowscn_bug_seq_nums_***REMOVED***x.csv' With CSV DELIMITER ','
+\copy (call ***x_duplicate_seq_nos()) To '/tmp/ams-reporting/12AZUREDB_AMD_ora_rowscn_bug_seq_nums_fix.csv' With CSV DELIMITER ','
