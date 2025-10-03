1bdad2e41e4de7a124e020c1b025c52ca77f4b45
djporta <157360643+djporta@users.noreply.github.com>
2025-10-03 15:10:05 +0100

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ############################################################### This is the AMD AzureDB HealthCheck script, and the associated documentation is in Ensemble under the "Libra System Admin Documents" area:
 ############################################################### "GoB Phase 1 - Oracle_Postgres DB Checks_v11.9_MAP.docx" is the latest version as of 27/02/2025
-echo "Script Version 27.7: no proxy"
+echo "Script Version 27.7: 105 Check #3 Tier 5"
 echo "Designed by Mark A. Porter"
 
 if [[ `echo $KV_NAME | grep "test"` ]];then
@@ -346,9 +346,9 @@ if [[ $schema_id == 77 ]];then
   backlog_adaptive_threshold=$(($backlog_adaptive_threshold*8))
 elif [[ $schema_id == 99 ]];then
   backlog_adaptive_threshold=$(($backlog_adaptive_threshold*5))
-elif [[ $schema_id == 38 ]] || [[ $schema_id == 135 ]];then
+elif [[ $schema_id == 38 ]] || [[ $schema_id == 135 ]] || [[ $schema_id == 105 ]];then
   backlog_adaptive_threshold=$(($backlog_adaptive_threshold*4))
-elif [[ $schema_id == 105 ]] || [[ $schema_id == 129 ]] || [[ $schema_id == 82 ]] || [[ $schema_id == 124 ]] || [[ $schema_id == 31 ]];then
+elif [[ $schema_id == 129 ]] || [[ $schema_id == 82 ]] || [[ $schema_id == 124 ]] || [[ $schema_id == 31 ]];then
   backlog_adaptive_threshold=$(($backlog_adaptive_threshold*3))
 elif [[ $schema_id == 130 ]] || [[ $schema_id == 126 ]] || [[ $schema_id == 112 ]] || [[ $schema_id == 47 ]] || [[ $schema_id == 36 ]] || [[ $schema_id == 103 ]] || [[ $schema_id == 106 ]] || [[ $schema_id == 61 ]];then
   backlog_adaptive_threshold=$(($backlog_adaptive_threshold*2))
