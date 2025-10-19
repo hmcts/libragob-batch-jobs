31cd32bdda708ed52c32714e39f108b85e194b46
djporta <157360643+djporta@users.noreply.github.com>
2025-10-19 18:57:19 +0100

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -190,7 +190,7 @@ cat ${OPDIR}onpremise_endpoint_check
   if [[ $onpremise_endpoint_check ]];then
     echo "$(date "+%d/%m/%Y %T"),AZDB_onpremise_endpoint_check,On-premise endpoint is reachable $onpremise_endpoint_check,ok" >> $OUTFILE
   else
-    echo "$(date "+%d/%m/%Y %T"),AZDB_onpremise_endpoint_check,On-premise endpoint is down so raise JIRA and get HMCTS PlatOps to investigate,warn" >> $OUTFILE
+    echo "$(date "+%d/%m/%Y %T"),AZDB_onpremise_endpoint_check,On-premise endpoint is down so check if the ManagedServers in IH are running,warn" >> $OUTFILE
   fi
 else
     echo "$(date "+%d/%m/%Y %T"),AZDB_onpremise_endpoint_check,There are no NodeJS PODs in Running status so if this is unexpected then raise JIRA and get HMCTS PlatOps to investigate,warn" >> $OUTFILE
