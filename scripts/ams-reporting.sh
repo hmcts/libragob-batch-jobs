8883cc479f766ae2918412ef1aad2161dd828fcb
djporta <157360643+djporta@users.noreply.github.com>
2026-02-26 17:15:25 +0000

    Update ams-reporting.sh

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ############################################################### This is the AMD AzureDB HealthCheck script, and the associated documentation is in Ensemble under the "Libra System Admin Documents" area:
 ############################################################### "GoB Phase 1 - Oracle_Postgres DB Checks_v11.9_MAP.docx" is the latest version as of 27/02/2025
-echo "Script Version 29.3: 4day rec check test"
+echo "Script Version 29.3: 4day rec check test2"
 echo "Designed by Mark A. Porter"
 # arbitrary script change to test if build action/checks trigger
 
@@ -816,7 +816,7 @@ fi
 echo "cat of ${OPDIR}9AZUREDB_AMD_confiscation_recon_result_by_met.csv"
 cat ${OPDIR}9AZUREDB_AMD_confiscation_recon_result_by_met.csv
 
-echo "op_date=$op-date"
+echo "op_date=$op_date"
 last_themis_rec_run_date=`head -1 ${OPDIR}9AZUREDB_AMD_confiscation_recon_result_by_met.csv | awk -F"," '{print $3}'`
 echo "last_themis_rec_run_date=$last_themis_rec_run_date"
 last_themis_rec_run_date_1900_secs=$(date '+%s' -d "$last_themis_rec_run_date")
