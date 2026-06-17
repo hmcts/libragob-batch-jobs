0917678212806457843e6b61ff6790cac14566fb
dstoodley1991 <daniel.stoodley@hmcts.net>
2026-06-17 15:06:03 +0100

    Update script version and echo message format

--- scripts/ams-reporting.sh
+++ scripts/ams-reporting.sh
@@ -2,8 +2,9 @@
 ############################################################### This is the AMD AzureDB HealthCheck script, and the associated documentation is in Ensemble under the "Libra System Admin Documents" area:
 ############################################################### "GoB Phase 1 - Oracle_Postgres DB Checks_v11.9_MAP.docx" is the latest version as of 27/02/2025
 echo "Script Version 29.4: TEST update RT"
-echo "Designed by Mark A. Porter"
+echo "Designed by Mark A. Porter -- "
 # arbitrary script change to test if build action/checks trigger
+
 
 OPDIR="/tmp/ams-reporting/"
 mkdir $OPDIR
